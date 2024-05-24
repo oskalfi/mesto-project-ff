999fa268754cab668aa106c84405dcff479bde51
Adam Stadnikov <stadnikov.adam@gmail.com>
2024-05-24 15:17:37 +0300

    Исправил имена переменных, добавил alt изображениям

--- scripts/index.js
+++ scripts/index.js
@@ -5,10 +5,11 @@ const cardTemplate = document.querySelector('#card-template').content;
 const placesList = document.querySelector('.places__list');
 
 // @todo: Функция создания карточки
-function createCard(initialCards, deleteCard) {
+function createCard(cardData, deleteCard) {
 	const card = cardTemplate.querySelector('.card').cloneNode(true);
-	card.querySelector('.card__image').src = `${initialCards.link}`;
-	card.querySelector('.card__title').textContent = `${initialCards.name}`;
+	card.querySelector('.card__image').src = cardData.link;
+	card.querySelector('.card__image').alt = cardData.name;
+	card.querySelector('.card__title').textContent = cardData.name;
 
 	// Повесили обработчик клика по иконке удаления карточки
 	const deleteButton = card.querySelector('.card__delete-button');
@@ -18,7 +19,7 @@ function createCard(initialCards, deleteCard) {
 }
 
 // @todo: Функция удаления карточки
-function deleteCard() {
+function deleteCard(event) {
 	//обращаемся к объекту события (иконке удаления) и вызываем на нём метод closest, 
 	// чтобы иметь возможность обратиться к карточке
 	const listItem = event.target.closest('.card');
@@ -26,10 +27,10 @@ function deleteCard() {
 }
 
 // @todo: Вывести карточки на страницу
-function displayCard() {
+function displayCards() {
 	for(const place of initialCards) {
 	placesList.append(createCard(place, deleteCard));
 	}
 }
 
-displayCard();
+displayCards();
